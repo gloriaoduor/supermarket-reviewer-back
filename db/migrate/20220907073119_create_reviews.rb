a6f12cd728087662c3a1bb50010e0f9f06819ffb
gloria oduor <gloriaoduor@gmail.com>
2022-09-07 10:35:20 +0300

    create reviews table

--- db/migrate/20220907073119_create_reviews.rb
+++ db/migrate/20220907073119_create_reviews.rb
@@ -1,4 +1,8 @@
 class CreateReviews < ActiveRecord::Migration[6.1]
   def change
+    create_table :reviews do |t|
+      t.string :comment
+      t.integer :review_id
+      t.integer :shopper_id 
   end
 end
