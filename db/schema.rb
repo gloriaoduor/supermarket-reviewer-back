630808a424d1c6cb9dc0583b272441a24b0e0835
gloria oduor <gloriaoduor@gmail.com>
2022-09-07 10:39:42 +0300

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2022_09_07_073153) do
+
+  create_table "reviews", force: :cascade do |t|
+    t.string "comment"
+    t.integer "review_id"
+    t.integer "shopper_id"
+  end
+
+  create_table "shoppers", force: :cascade do |t|
+    t.string "name"
+  end
+
+  create_table "supermarkets", force: :cascade do |t|
+    t.string "name"
+  end
 
 end
