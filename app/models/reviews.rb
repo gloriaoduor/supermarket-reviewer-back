2371cdfd5902368caee06f32370ba3fb445b6259
gloria oduor <gloriaoduor@gmail.com>
2022-09-07 10:52:05 +0300

    defined associations

--- app/models/reviews.rb
+++ app/models/reviews.rb
@@ -1,3 +1,4 @@
 class Reviews < ActiveRecord::Base 
-    
+    belongs_to :supermarket
+    belongs_to :shopper
 end 
--- app/models/shoppers.rb
+++ app/models/shoppers.rb
@@ -1,3 +1,4 @@
 class Shoppers < ActiveRecord::Base 
-    
+    has_many :reviews 
+    has_many :supermarkets, through: :reviews 
 end 
