cae458b4f5e95c87cf2476045c99bbc9dd65d7e9
gloria oduor <gloriaoduor@gmail.com>
2022-09-12 09:54:13 +0300

    change routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,21 @@
 
+require 'sinatra/cross_origin'
 class ApplicationController < Sinatra::Base
+
+    set :default_content_type, 'application/json'
+
+    configure do
+        enable :cross_origin
+      end
+      before do
+        response.headers['Access-Control-Allow-Origin'] = '*'
+      end
     get '/' do 
-        "<h1>Homepage </h1>"
-    end
-    #when one wants to see all supermarkets (how to render to frontend)
-    get '/supermarkets' do
-        s_markets = Supermarket.all
-        s_markets.to_json
+      sm = Supermarket.all
+      sm.to_json(only: [:id, :name],
+      include: {reviews: {only: [:id, :comment],
+      include: {shopper: {only: [:id, :name]}}}
+      })
     end 
     #get all reviews of the supermarket that has been specified
     get '/:name' do
@@ -15,6 +24,21 @@ class ApplicationController < Sinatra::Base
                     include: {reviews: {only: [:comment],
                     include: {shopper: {only: [:name]}}}
                     })
+    end 
 
+    post '/newreview' do
+      shopper = Shopper.create(name: params[:name])
+      shopper.to_json
+      review =  Review.create(comment: params[:comment], shopper_id: shopper.id, supermarket_id: params[:supermarket_id])
+      review.to_json
+      #how to get the id of the supermarket from a dropdown
+    end 
+
+
+    options "*" do
+        response.headers["Allow"] = "GET, PUT, POST, DELETE, OPTIONS"
+        response.headers["Access-Control-Allow-Headers"] = "Authorization, Content-Type, Accept, X-User-Email, X-Auth-Token"
+        response.headers["Access-Control-Allow-Origin"] = "*"
+        200
     end 
 end 
